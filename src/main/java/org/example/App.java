3569f41255574e6c483991b2dd2a44e32bc75f3d
ritaf <ritastefania.franco@yahoo.it>
2020-10-28 11:43:59 +0100

    first version

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,13 +2,11 @@ package org.example;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        Integer number=20;
+    public static void main( String[] args ) {
+        Integer number = 20;
         Math m = new Math();
 
-
         m.generatoreNumeriPrimi(number);
-
+    }
 
 }
